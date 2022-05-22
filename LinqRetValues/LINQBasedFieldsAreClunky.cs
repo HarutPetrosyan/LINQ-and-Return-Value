cf49def90c4d40a42189b44fd58f176831f22732
Harut <43827108+Harut879794@users.noreply.github.com>
2022-05-22 11:08:13 +0400

    Add project files.

---
+++ LinqRetValues/LINQBasedFieldsAreClunky.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqRetValues
+{
+    internal class LINQBasedFieldsAreClunky
+    {
+        private static string[] currentVideoGames = {"Morrowind", "Uncharted 2",
+"Fallout 3", "Daxter", "System Shock 2"};
+        private IEnumerable<string> videoGames= from g in currentVideoGames where g.Contains(" ") orderby g select g;
+        public void PrintGame()
+        {
+            foreach (var item in videoGames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ LinqRetValues/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqRetValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> list = GetStringSubset();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            var str=GetstringSubsetArray();
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        static IEnumerable<string > GetStringSubset()
+        {
+            string[] result = { "BWM", "Mersedes Benz", "Audi", "Rolce Royce", "Aston Martin", "Uaguar" };
+            IEnumerable<string> result2 = from i in result orderby i select i;
+            return result2;
+        }
+        static string[] GetstringSubsetArray()
+        {
+            string[] result = { "BWM", "Mersedes Benz", "Audi", "Rolce Royce", "Aston Martin", "Uaguar" };
+            var subset= from i in result orderby i select i;
+            return subset.ToArray();
+        }
+    }
+}
